208e7fe8abdfdbd7bdc823931d172ce8e7edc81c
Mayursinh <marvinduff97@gmail.com>
2020-01-12 21:34:49 +0530

    implement cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const debug = require('debug')('app');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const apis = require('./API/analyze.api');
+
+app.use(cors());
 
 app.use('/api', apis);
 
