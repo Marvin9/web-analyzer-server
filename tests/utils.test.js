7f68370dd5528d4b2acf9c606a80fde252fd71d4
Mayursinh <marvinduff97@gmail.com>
2020-01-22 17:21:47 +0530

    add tests for sanitizeUrl function of utils.js

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -15,3 +15,47 @@ describe('keyExists', () => {
     expect(utils.keyExists(obj, 'anotherKey')).toBeTruthy();
   });
 });
+
+const checkForSameInputAndOutput = (inputs) => {
+  inputs.forEach((input) => {
+    expect(utils.sanitizeUrl(input)).toBe(input);
+  });
+};
+
+describe('sanitizeUrl', () => {
+  const standardGithubURL = 'https://www.github.com';
+  const standardGithubURLWithPath = 'https://www.github.com/Marvin9';
+  const standardGithubURLWithQuery = 'https://www.github.com/Marvin9?tab=repositories';
+
+  it('should return empty string if url is invalid', () => {
+    expect(utils.sanitizeUrl('invalidurl')).toBe('');
+  });
+
+  it('should return correct string for url containing -> protocol + subdomain + domain', () => {
+
+    checkForSameInputAndOutput([standardGithubURL, standardGithubURLWithPath, standardGithubURLWithQuery]);
+  });
+
+  it('should return correct string (with subdomain as "www") for url containing -> protocol + domain', () => {
+    const url = 'https://github.com';
+    const urlWithPath = 'https://github.com/Marvin9';
+    const urlWithQuery = 'https://github.com/Marvin9?tab=repositories';
+
+    expect(utils.sanitizeUrl(url)).toBe(standardGithubURL);
+    expect(utils.sanitizeUrl(urlWithPath)).toBe(standardGithubURLWithPath);
+    expect(utils.sanitizeUrl(urlWithQuery)).toBe(standardGithubURLWithQuery);
+  });
+
+  it('should return correct string (with protocol as "https") for url containing -> subdomain + domain', () => {
+    const url = 'www.github.com';
+    const url2 = 'www.google.com';
+
+    expect(utils.sanitizeUrl(url)).toBe(standardGithubURL);
+  });
+
+  it('should return correct string (with protocol as "https" & subdomain as "www") for url containing -> domain', () => {
+    const url = 'github.com';
+
+    expect(utils.sanitizeUrl(url)).toBe(standardGithubURL);
+  });
+});
