689533b2c9326c786b0097a987fc38ba4fbea79c
Mayursinh <marvinduff97@gmail.com>
2020-01-22 16:56:40 +0530

    sanitize url function, to change incomplete url to complete ones, following standard protocol and subdomain name

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const parse5 = require('parse5');
+const urlUtil = require('url');
 
 const generateTree = (url, cb) => {
   request(url, (err, res, body) => {
@@ -20,8 +21,36 @@ const objectOfPlugin = (plugin) => ({
   example: plugin.example,
 });
 
+
+const sanitizeUrl = (url) => {
+  const protocolSubdomainDomain = /http(s)?:\/\/\w+\.\w+\.\w+/i;
+  const protocolDomain = /http(s)?:\/\/\w+\.\w+/i;
+  const subdomainDomain = /\w+\.\w+\.\w+/i;
+  const domain = /\w+\.\w+/i;
+
+  if (protocolSubdomainDomain.test(url)) {
+    return url;
+  }
+
+  if (protocolDomain.test(url)) {
+    const sanitizedUrl = urlUtil.parse(url);
+    return `${sanitizedUrl.protocol}//www.${sanitizedUrl.host}${sanitizedUrl.path}`;
+  }
+
+  if (subdomainDomain.test(url)) {
+    return `https://${url}`;
+  }
+
+  if (domain.test(url)) {
+    return `https://www.${url}`;
+  }
+
+  return '';
+};
+
 module.exports = {
   generateTree,
   keyExists,
   objectOfPlugin,
+  sanitizeUrl,
 };
