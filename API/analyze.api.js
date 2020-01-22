4a85dd50acbc226d8c2e80252636be0f38969932
Mayursinh <marvinduff97@gmail.com>
2020-01-22 16:57:26 +0530

    use sanitized url to accept urls like 'google.com/somethin' or 'github.com'

--- API/analyze.api.js
+++ API/analyze.api.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const analyzer = express.Router();
 const Analyzer = require('../helpers/Analyzer');
-const { generateTree } = require('../helpers/utils');
+const { generateTree, sanitizeUrl } = require('../helpers/utils');
 
 const an = new Analyzer();
 
@@ -10,29 +10,37 @@ analyzer.get('/analyze', (req, res) => {
   an.report = [];
   const url = req.query || null;
   // todo validate url
-
-  generateTree(url, (err, tree) => {
-    if (err) {
-      if (err.error) {
-        res.send({
-          error: true,
-          desc: 'Url was forbidden',
-          code: 200,
-        });
+  if (url.url.length > 2048) {
+    res.send({
+      error: true,
+      code: 404,
+      desc: 'Url length should not exceed 2048 characters',
+    });
+  } else {
+	const sanitizedUrl = sanitizeUrl(url.url);
+    generateTree(sanitizedUrl, (err, tree) => {
+      if (err) {
+        if (err.error) {
+          res.send({
+            error: true,
+            desc: 'Url was forbidden',
+            code: 200,
+          });
+        } else {
+          res.status(404).send({
+            error: true,
+            code: 404,
+            desc: 'Internal error',
+            err,
+          });
+        }
       } else {
-        res.status(404).send({
-          error: true,
-          code: 404,
-          desc: 'Internal error',
-          err,
+        an.reporter(tree, sanitizedUrl, () => {
+          res.send(an.report);
         });
       }
-    } else {
-      an.reporter(tree, url.url, () => {
-        res.send(an.report);
-      });
-    }
-  });
+    });
+  }
 });
 
 module.exports = analyzer;
